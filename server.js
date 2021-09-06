c49d8b258632bab5f6f3661f1db6b2bc7163b5be
abhishekpanditofficial <abhishekpanditoff@gmail.com>
2021-09-07 00:47:21 +0530

    added cloud mongodb

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express()
+const PORT= process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost/urlShortener', {
+const DB= process.env.DATABASE_URL.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
+ 
+
+mongoose.connect(DB, {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
@@ -31,4 +36,6 @@ app.get('/:shortUrl', async (req, res) => {
   res.redirect(shortUrl.full)
 })
 
-app.listen(process.env.PORT || 5000);
+app.listen(PORT, () =>{
+  console.log('Server started');
+});
